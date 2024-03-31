950cbbcf2d057a48ef221f6fe786e10bd00f860e
vinodpatil2002 <vinodnspatil@gmail.com>
2024-04-01 00:10:41 +0530

    Caching data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,23 +1,28 @@
-import { link } from 'fs';
-import React from 'react'
+import React from "react";
 
 interface User {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 }
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
-  const users: User[] = await res.json()
+    const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+        next: {
+            revalidate: 10,
+        },
+    });
+    const users: User[] = await res.json();
 
-  return (
-    <>
-      <h1>Users</h1>
-      <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
-      </ul>
-    </>
-    )
-}
+    return (
+        <>
+            <h1>Users</h1>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </>
+    );
+};
 
-export default UsersPage
+export default UsersPage;
